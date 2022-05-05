f3453c5d7d81230c482080d97db06da08d393fe8
Alien Ware <jacmg01@gmail.com>
2022-05-05 01:32:40 -0600

    Calculadora

--- src/components/Calculadora/Calcu.jsx
+++ src/components/Calculadora/Calcu.jsx
@@ -3,30 +3,30 @@ import React from "react";
 import "./Calcu.scss";
 
 export default function Calcu() {
- 
+
   return (
    <>
     <div className="container">
-        <div className = "display"></div>
-        <button className="c"></button>
-        <button className="masmenos"></button>
-        <button className="porciento"></button>
-        <button className="division"></button>
-        <button className="siete"></button>
-        <button className="ocho"></button>
-        <button className="nueve"></button>
-        <button className="por"></button>
-        <button className="cuatro"></button>
-        <button className="cinco"></button>
-        <button className="seis"></button>
-        <button className="menos"></button>
-        <button className="uno"></button>
-        <button className="dos"></button>
-        <button className="tres"></button>
-        <button className="mas"></button>
-        <button className="cero"></button>
-        <button className="punto"></button>
-        <button className="igual"></button>
+        <div className = "display">000000000</div>
+        <button className="func c">C</button>
+        <button className="func masmenos">⁺∕₋</button>
+        <button className="func porciento">%</button>
+        <button className="oper division">/</button>
+        <button className="keyboard siete">7</button>
+        <button className="keyboard ocho">8</button>
+        <button className="keyboard nueve">9</button>
+        <button className="oper por">x</button>
+        <button className="keyboard cuatro">4</button>
+        <button className="keyboard cinco">5</button>
+        <button className="keyboard seis">6</button>
+        <button className="oper menos">-</button>
+        <button className="keyboard uno">1</button>
+        <button className="keyboard dos">2</button>
+        <button className="keyboard tres">3</button>
+        <button className="oper mas">+</button>
+        <button className="keyboard cero">0</button>
+        <button className="keyboard punto">.</button>
+        <button className="keyboard igual" >=</button>
         
         
     </div>
