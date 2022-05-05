8c8e3eca3749b289da122226404b8312472dcfc5
Alien Ware <jacmg01@gmail.com>
2022-05-05 00:44:03 -0600

    forma hecha

---
+++ src/components/Calculadora/Calcu.jsx
@@ -0,0 +1,36 @@
+
+import React from "react";
+import "./Calcu.scss";
+
+export default function Calcu() {
+ 
+  return (
+   <>
+    <div className="container">
+        <div className = "display"></div>
+        <button className="c"></button>
+        <button className="masmenos"></button>
+        <button className="porciento"></button>
+        <button className="division"></button>
+        <button className="siete"></button>
+        <button className="ocho"></button>
+        <button className="nueve"></button>
+        <button className="por"></button>
+        <button className="cuatro"></button>
+        <button className="cinco"></button>
+        <button className="seis"></button>
+        <button className="menos"></button>
+        <button className="uno"></button>
+        <button className="dos"></button>
+        <button className="tres"></button>
+        <button className="mas"></button>
+        <button className="cero"></button>
+        <button className="punto"></button>
+        <button className="igual"></button>
+        
+        
+    </div>
+
+   </>
+  );
+}
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+
+import "./main.scss";
+import React from "react";
+import ReactDom from "react-dom";
+import Calcu from "./components/Calculadora/Calcu.jsx";
+
+ReactDom.render(
+  <Calcu />,
+  document.getElementById("root"),
+
+);
