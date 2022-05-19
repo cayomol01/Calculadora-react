e014c593ad57c882d81165659a58ea3137112d25
Alien Ware <jacmg01@gmail.com>
2022-05-19 07:11:29 -0600

    Adding tests

---
+++ src/components/Calculadora/Calcu.test.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import React from "react"
+import ReactDOM from "react-dom"
+import {
+  screen, render, act, fireEvent,
+} from "@testing-library/react"
+import "@testing-library/jest-dom/extend-expect"
